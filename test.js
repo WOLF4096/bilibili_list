98a26a7aa0d134484995029463b81386ab7e4e03
狼介 <wolf4096@foxmail.com>
2022-05-22 19:34:33 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,45 @@
+uid = 553402409;
+num = 50
+url = "https://api.bilibili.com/x/relation/followers?vmid=" + uid + "&pn=1";//粉丝
+url = "https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=1";//关注
+
+httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', url, true);
+httpRequest.send();
+httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        json = httpRequest.responseText;
+        obj = JSON.parse(json);
+        zon = obj.data.total;
+    }
+};
+// console.log(zon);
+var e = 0;
+var n = 0;
+for (i = 0; i < zon;i = i + num) {
+    e++;
+    url = "https://api.bilibili.com/x/relation/followers?vmid=" + uid + "&pn=" + e + "&ps=" + num + "&order=desc&order_type=attention&jsonp=jsonp";//粉丝
+    url = "https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=" + e + "&ps=" + num + "&order=desc&order_type=attention&jsonp=jsonp";//关注
+    jsonlist(url);
+}
+function jsonlist(url){
+    httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            json = httpRequest.responseText;
+            obj = JSON.parse(json);
+            console.log(url);
+            zon = obj.data.list.length;
+            for (o = 0; o < zon; o++) {
+                n++
+                uid = obj.data.list[o].mid;
+                nam = obj.data.list[o].uname;
+                sig = obj.data.list[o].sign.replace(/[\r\n]/g,"");
+                fac = obj.data.list[o].face;
+                console.log(n + "," + uid + "," + nam + "," + sig + "," + fac);
+            }
+        }
+    };    
+}
